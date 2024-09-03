d3c40e958aa9be30aca0bc14daa3082243b57f85
Douglas Jasper <doug.jasper@trios.com>
2024-09-03 19:15:26 -0400

    End of Chapter 20

--- podplay/app/src/main/java/com/trios2024evdj/podplay/ui/PodcastActivity.kt
+++ podplay/app/src/main/java/com/trios2024evdj/podplay/ui/PodcastActivity.kt
@@ -1,18 +1,36 @@
 package com.trios2024evdj.podplay.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.trios2024evdj.podplay.R
+import com.trios2024evdj.podplay.repository.ItunesRepo
+import com.trios2024evdj.podplay.service.ItunesService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PodcastActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_podcast)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm("Android Developer")
+            Log.i(TAG, "Results = ${results.body()}")
+            }
+
+
+                    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
