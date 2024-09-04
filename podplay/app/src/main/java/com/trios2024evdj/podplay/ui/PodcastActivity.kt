9c64fb7c5937f637e2011b6683cf58c02e5a9641
Douglas Jasper <doug.jasper@trios.com>
2024-09-03 20:32:46 -0400

    Search Results

--- podplay/app/src/main/java/com/trios2024evdj/podplay/ui/PodcastActivity.kt
+++ podplay/app/src/main/java/com/trios2024evdj/podplay/ui/PodcastActivity.kt
@@ -1,7 +1,12 @@
 package com.trios2024evdj.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -36,4 +41,44 @@ class PodcastActivity : AppCompatActivity() {
             insets
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // 1
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        // 2
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        // 3
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        // 4
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+
 }
