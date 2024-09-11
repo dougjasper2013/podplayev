448a93d09b80f55cbedf6b61b44302aa8b613321
Douglas Jasper <doug.jasper@trios.com>
2024-09-11 19:51:33 -0400

    Podcast Playing

--- podplay/app/src/main/java/com/trios2024evdj/podplay/service/PodplayMediaService.kt
+++ podplay/app/src/main/java/com/trios2024evdj/podplay/service/PodplayMediaService.kt
@@ -17,12 +17,18 @@ class PodplayMediaService : MediaBrowserServiceCompat() {
     override fun onLoadChildren(parentId: String,
                                 result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
         // To be implemented
+        if (parentId.equals(PODPLAY_EMPTY_ROOT_MEDIA_ID)) {
+            result.sendResult(null)
+        }
+
     }
 
     override fun onGetRoot(clientPackageName: String,
                            clientUid: Int, rootHints: Bundle?): BrowserRoot? {
         // To be implemented
-        return null
+        return BrowserRoot(
+            PODPLAY_EMPTY_ROOT_MEDIA_ID, null)
+
     }
 
     private fun createMediaSession() {
@@ -35,6 +41,11 @@ class PodplayMediaService : MediaBrowserServiceCompat() {
         val callback = PodplayMediaCallback(this, mediaSession)
         mediaSession.setCallback(callback)
 
+    }
+
+    companion object {
+        private const val PODPLAY_EMPTY_ROOT_MEDIA_ID =
+            "podplay_empty_root_media_id"
     }
 
 
